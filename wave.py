431fed335f5d4f6b47a3ba8cbe837798480287d3
CreatorOfThing <76164167+CreatorOfThing@users.noreply.github.com>
2022-02-01 22:16:22 +0300

    Create wave.py

---
+++ wave.py
@@ -0,0 +1,18 @@
+import sleep from time
+
+
+zombies = 0
+max_zombies = 15
+wave = 0
+
+while true:
+  
+  if zombies < max_zombies:
+    wait(150)
+    zombies += 5
+    wave += 1
+
+   
+    
+    
+  
